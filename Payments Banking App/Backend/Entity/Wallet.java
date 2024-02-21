bb6eefbca402678a8d39165848a91e8443de67d3
THE_VIJAY <vijaykumarmetoo@gmail.com>
2024-02-21 19:54:08 +0530

    Wallet

---
+++ Payments Banking App/Backend/Entity/Wallet.java
@@ -0,0 +1,27 @@
+
+
+public class Wallet {
+    private int defaultBal;
+	private int userId;
+	private int limit;
+	public int getLimit() {
+		return limit;
+	}
+	public void setLimit(int limit) {
+		this.limit = limit;
+	}
+	public int getDefaultBal() {
+		return defaultBal;
+	}
+	public void setDefaultBal(int defaultBal) {
+		this.defaultBal = defaultBal;
+	}
+	public int getUserId() {
+		return userId;
+	}
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	
+}
