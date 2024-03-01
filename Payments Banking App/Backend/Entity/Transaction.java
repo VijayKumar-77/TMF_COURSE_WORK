03e28c3ce19ca4e26c6d2e6bfb7a4e2668214244
Vijay Kumar <vijaykumarmetoo@gmail.com>
2024-03-01 18:26:37 +0530

    Transaction

--- Payments Banking App/Backend/Entity/Transaction.java
+++ Payments Banking App/Backend/Entity/Transaction.java
@@ -1,4 +1,46 @@
 
-public enum Transaction {
+public class Transaction {
+private int trnxnId;
+private double trnxnAmt;
+private TransactionSource Trnxsrc;
+private long Srcnumber;
+private TransactionDestination TrnxDest;
+private long Destnumber;
+public int getTrnxnId() {
+	return trnxnId;
+}
+public void setTrnxnId(int trnxnId) {
+	this.trnxnId = trnxnId;
+}
+public double getTrnxnAmt() {
+	return trnxnAmt;
+}
+public void setTrnxnAmt(double trnxnAmt) {
+	this.trnxnAmt = trnxnAmt;
+}
+public TransactionSource getTrnxsrc() {
+	return Trnxsrc;
+}
+public void setTrnxsrc(TransactionSource trnxsrc) {
+	Trnxsrc = trnxsrc;
+}
+public long getSrcnumber() {
+	return Srcnumber;
+}
+public void setSrcnumber(long srcnumber) {
+	Srcnumber = srcnumber;
+}
+public TransactionDestination getTrnxDest() {
+	return TrnxDest;
+}
+public void setTrnxDest(TransactionDestination trnxDest) {
+	TrnxDest = trnxDest;
+}
+public long getDestnumber() {
+	return Destnumber;
+}
+public void setDestnumber(long destnumber) {
+	Destnumber = destnumber;
+}
 
 }
