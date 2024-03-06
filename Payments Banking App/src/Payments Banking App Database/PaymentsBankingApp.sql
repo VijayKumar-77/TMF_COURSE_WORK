4f51fd73bd0ff3177ca45dc371d107dc2632db14
Vijay Kumar <vijaykumarmetoo@gmail.com>
2024-03-06 19:55:59 +0530

    PBA SQL File

--- Payments Banking App/src/Payments Banking App Database/PaymentsBankingApp.sql
+++ Payments Banking App/src/Payments Banking App Database/PaymentsBankingApp.sql
@@ -2,16 +2,16 @@ create database PaymentsBankingApp;
 
 show databases;
 
-use 
-PaymentsBankingApp;
+use PaymentsBankingApp;
 
 create table User(
 UserID      int not null,
 FirstName   varchar(10),
 LastName    varchar(10),
-PhNo        long,
+PhoneNum    long,
 DOB         date,
 Address     varchar(50),
+Wallet      int not null,
 Password    varchar(10),
 primary key (UserID)
 );
@@ -29,27 +29,27 @@ references       User(UserID)
 desc Wallet;
 
 create table BankAccount(
-UserID          int not null,
-AccountNumber   int not null,
-BankName        varchar(20),
-IFSCCODE        varchar(10),
-AccountType     varchar(10),
-BankAccountPin  varchar(10),
-primary key     (AccountNumber),
-foreign key     (UserID)
-references       User (UserID));
+UserID              int not null,
+BankAccountNumber   int not null,
+BankAccountName     varchar(20),
+IFSCNumber          varchar(10),
+BankAccountType     varchar(10),
+BankAccountPin      varchar(10),
+primary key        (BankAccountNumber),
+foreign key        (UserID)
+references          User (UserID));
 
 desc BankAccount;
 
 create table Transaction(
-UserID                 int not null,
-TransactionID          int not null,
-TransactionSource      varchar(20),
-TransactionDestination varchar(20),
-TransactionAmount      double,
-primary key            (TransactionID),
-foreign key            (UserID)
-references             User (UserID)
+UserID           int not null,
+TrnxnID          int not null,
+TrnxnAmount      double,
+TrnxnSource      varchar(20),
+TrnxDestination  varchar(20),
+primary key      (TrnxnID),
+foreign key      (UserID)
+references        User (UserID)
 );
 
 desc Transaction;
