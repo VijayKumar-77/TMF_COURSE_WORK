c24a99001cf53e407eb047d66a5f7814731f4c0f
Vijay Kumar <vijaykumarmetoo@gmail.com>
2024-03-10 21:55:35 +0530

    String Vowel or consonant

---
+++ Java Programs/String Vowel or consonant.java
@@ -0,0 +1,19 @@
+int main()
+{
+    char c='F';
+
+    
+        
+    //checking for vowels	
+    if(c=='a'||c=='e'||c=='i'||c=='o'||c=='u'||
+    c=='A'||c=='E'||c=='I'||c=='O'||c=='U')
+    {
+        printf("%c is a vowel", c); 
+    }
+    else
+    {
+        printf("%c is a consonant", c); 
+    }
+
+    return 0;
+}
