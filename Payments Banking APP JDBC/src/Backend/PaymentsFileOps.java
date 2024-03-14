471153390525df92a8e64fa634d29214317c8926
Vijay Kumar <vijaykumarmetoo@gmail.com>
2024-03-14 08:16:17 +0530

    PaymentsFileOps

---
+++ Payments Banking APP JDBC/src/Backend/PaymentsFileOps.java
@@ -0,0 +1,38 @@
+package payments_app_cli;
+
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import payments_app_cli.entity.User;
+
+
+public class PaymentsFileOps {
+	public final String usersFilePath = "C:\\Users\\vijay\\Downloads\\Paymnets Main\\Payments_CLI_USers.csv";
+	
+	public void writeUserToFile(User u) throws IOException {
+		
+		File userFile = new File(usersFilePath);
+		FileWriter fw = new FileWriter(userFile,true);
+		fw.write(u.userToFileRecord());
+		
+		
+		String a="";
+		List<String> los=new ArrayList<String>();
+		FileReader f2=new FileReader(userFile);
+		BufferedReader b=new BufferedReader(f2);
+		while((a=b.readLine())!=null)
+		{
+			los.add(a);
+		}
+		fw.flush();
+		fw.close();
+	}
+
+}
