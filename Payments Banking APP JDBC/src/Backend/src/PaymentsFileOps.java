0e767a5d77fb54a6b5899c7645c7c6ee31d06b31
Vijay Kumar <vijaykumarmetoo@gmail.com>
2024-03-19 09:31:19 +0530

    PaymentsFileOps

---
+++ Payments Banking APP JDBC/src/Backend/src/PaymentsFileOps.java
@@ -0,0 +1,38 @@
+
+
+
+
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+
+
+
+public class PaymentsFileOps {
+	public final String usersFilePath = "C:\\Users\\DT-DELL-83\\Desktop\\TMF8577\\Payments_CLI_USers.csv";
+	
+	public void writeUserToFile(User u) throws IOException {
+		
+		File userFile = new File(usersFilePath);
+		FileWriter fw = new FileWriter(userFile,true);
+		fw.write(u.userToFileRecord());
+		
+		
+		String a="";
+		List<String> los=new ArrayList<String>();
+		FileReader f2=new FileReader(userFile);
+		BufferedReader b=new BufferedReader(f2);
+		while((a=b.readLine())!=null)
+		{
+			los.add(a);
+		}
+		fw.flush();
+		fw.close();
+	}
+
+}
